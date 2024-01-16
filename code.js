9f15bec8bf32d5cae3abbefb2d664db8f5f85ca6
justarandomaccountt <124544630+justarandomaccountt@users.noreply.github.com>
2024-01-16 07:20:20 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,13 @@
+const code = "123";
+
+const codesender = http.createServer((req, res) => {
+    if (req.method == "GET")
+    {
+        res.write(code);
+        res.end();
+    }
+    else {
+        res.statusCode = 404;
+        res.end();
+    }
+});
