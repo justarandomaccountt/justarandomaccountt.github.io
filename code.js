5604f2df566733aabbab556476b119647dc94885
justarandomaccountt <124544630+justarandomaccountt@users.noreply.github.com>
2024-01-16 12:30:36 -0500

    Initial commit

--- code.js
+++ code.js
@@ -8,7 +8,7 @@ xhr.open('GET', url, true);
 xhr.onload = function () {
     if (xhr.status >= 200 && xhr.status < 300)
     {
-        console.log(xhr.responseText.split(";")[1]);
+        console.log(xhr.responseText.slice(16, 56));
     }
     else {
         console.error(xhr.statusText);
